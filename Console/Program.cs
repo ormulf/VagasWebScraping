c789ca312935d7e9a5e6ce015b9e0499574fedca
[ormulf] <[ormulf@gmail.com]>
2025-11-10 09:05:34 -0300

    selenium chrome

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium.Chrome;
+
 Console.WriteLine("Hello, World!");
+var driver = new ChromeDriver();
+driver.Navigate().GoToUrl("https://www.linkedin.com/jobs/?focusToMoreMenuTrigger=true");
+Console.ReadKey();
+driver.Quit();
